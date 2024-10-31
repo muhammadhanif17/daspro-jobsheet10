b3d51231492040e1fbc10a24617b7a4dbfb6cc4b
Muhammad Hanif <hanifmuhammad1309@gmail.com>
2024-10-31 15:41:00 +0700

    p

---
+++ SearchNilai14.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SearchNilai14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int banyakNilai = sc.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
+        int key, hasil = 0;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i;
+                System.out.println();
+                System.out.println("Nilai yang dicari ketemu, " + key + " merupakan nilai mahasiswa ke-" + (hasil+1));
+                break;
+            }else {
+                System.out.println();
+                System.out.println("Nilai yang dicari tidak ditemukan");
+                break;
+            }
+        }
+    }
+}
